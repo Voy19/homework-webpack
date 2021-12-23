0a6fe09b33e757d704026edd59d8dfe15347795d
unknown <denys.voytov@gmail.com>
2021-12-23 14:23:36 +0200

    change

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,14 @@ dotenv.config();
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-module.exports = {
+module.exports = [{
+  name: 'old-browsers',
   context: path.resolve(__dirname, 'src'),
   mode: process.env.NODE_ENV,
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: '[name].bundle.js',
+    filename: '[name]-old-browsers.bundle.js',
   },
   resolve: {
     extensions: ['.js', '.json', '.jsx'],
@@ -56,7 +57,12 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/i,
@@ -99,4 +105,106 @@ module.exports = {
       },
     ],
   },
-};
+},
+{
+  name: 'new-browsers',
+  context: path.resolve(__dirname, 'src'),
+  mode: process.env.NODE_ENV,
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name]-new-browsers.bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, './public'),
+    },
+    open: true,
+    compress: true,
+    port: 3000,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
+    }),
+    ...(isProduction
+      ? [new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+        chunkFilename: '[name].[contenthash].css',
+      })]
+      : []),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                useBuiltIns: 'usage',
+                targets: { esmodules: true },
+                modules: false,
+              }],
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      // css
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /\.module\.css$/,
+      },
+      // css modules
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      // less
+      {
+        test: /\.less$/i,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      // scss/sass
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+}];
