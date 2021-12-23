15cc0bc9339ec94db15afca7831687ad164c843f
unknown <denys.voytov@gmail.com>
2021-12-23 06:44:36 +0200

    change babel settings

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.warn('test');
+import test from './test';
+
+console.warn(test());
+
+function sortStrings(strings) {
+  return strings.sort();
+}
+
+function createResolvedPromise() {
+  return Promise.resolve();
+}
